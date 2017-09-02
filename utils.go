1ec4315a576618c826fca65c48170f6c9e325449
Tietang Wang <tietang@MacBook-Pro.local>
2017-09-02 23:22:23 +0800

    add hex 2 byte

--- utils.go
+++ utils.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"net"
 	"strings"
+	"bytes"
+	"strconv"
 )
 
 func Panic(err error) {
@@ -73,4 +75,31 @@ func GetExternalIP() (string, error) {
 	}
 	return "", errors.New("are you connected to the network?")
 }
+//byte转16进制字符串
+func ByteToHex(data []byte) string {
+	buffer := new(bytes.Buffer)
+	for _, b := range data {
 
+		s := strconv.FormatInt(int64(b&0xff), 16)
+		if len(s) == 1 {
+			buffer.WriteString("0")
+		}
+		buffer.WriteString(s)
+	}
+
+	return buffer.String()
+}
+
+//16进制字符串转[]byte
+func HexToBye(hex string) []byte {
+	length := len(hex) / 2
+	slice := make([]byte, length)
+	rs := []rune(hex)
+
+	for i := 0; i < length; i++ {
+		s := string(rs[i*2 : i*2+2])
+		value, _ := strconv.ParseInt(s, 16, 10)
+		slice[i] = byte(value & 0xFF)
+	}
+	return slice
+}
