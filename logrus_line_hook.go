ce7fd93ffc58f281a24c82690d3f06545d9d4c04
tietang <wtt.2012@gmail.com>
2019-01-26 21:48:28 +0800

    add readme

--- logrus_line_hook.go
+++ logrus_line_hook.go
@@ -1,10 +1,10 @@
 package utils
 
 import (
-    "fmt"
-    log "github.com/sirupsen/logrus"
-    "runtime"
-    "strings"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"runtime"
+	"strings"
 )
 
 //logrus 代码日志文件，函数和代码行位置输出hook
@@ -14,82 +14,90 @@ import (
 //lfh.EnableLogFuncName = true
 //log.AddHook(lfh)
 type LineNumLogrusHook struct {
-    //启用文件名称log
-    EnableFileNameLog bool
-    //启用函数名称log
-    EnableFuncNameLog bool
-    Skip              int
+	//启用文件名称log
+	EnableFileNameLog bool
+	//启用函数名称log
+	EnableFuncNameLog bool
+	SkipPrefixs       []string
+
+	Skip int
 }
 
 func NewLineNumLogrusHook() *LineNumLogrusHook {
-    return &LineNumLogrusHook{
-        EnableFileNameLog: true,
-        EnableFuncNameLog: true,
-        Skip:              5,
-    }
+	return &LineNumLogrusHook{
+		EnableFileNameLog: true,
+		EnableFuncNameLog: true,
+		Skip:              5,
+	}
 }
 
 func (hooks LineNumLogrusHook) Levels() []log.Level {
-    return log.AllLevels
+	return log.AllLevels
 }
 
 func (hook *LineNumLogrusHook) Fire(entry *log.Entry) error {
 
-    file, function, line := hook.findCaller(hook.Skip)
+	file, function, line := hook.findCaller(hook.Skip)
 
-    if hook.EnableFileNameLog && hook.EnableFuncNameLog {
-        entry.Message = fmt.Sprintf("[%s(%s:%d)] [%s]", function, file, line, entry.Message)
-    }
-    //router/route_table.go(43)
-    if hook.EnableFileNameLog && !hook.EnableFuncNameLog {
-        entry.Message = fmt.Sprintf("[%s(%d)] %s", file, line, entry.Message)
-    }
-    //microservice-gateway/v1/router.(*RouteTable).AddRoutePattern(43)
-    if !hook.EnableFileNameLog && hook.EnableFuncNameLog {
-        entry.Message = fmt.Sprintf("[%s(%d)] %s", function, line, entry.Message)
-    }
+	if hook.EnableFileNameLog && hook.EnableFuncNameLog {
+		entry.Message = fmt.Sprintf("[%s(%s:%d)] [%s]", function, file, line, entry.Message)
+	}
+	//router/route_table.go(43)
+	if hook.EnableFileNameLog && !hook.EnableFuncNameLog {
+		entry.Message = fmt.Sprintf("[%s(%d)] %s", file, line, entry.Message)
+	}
+	//microservice-gateway/v1/router.(*RouteTable).AddRoutePattern(43)
+	if !hook.EnableFileNameLog && hook.EnableFuncNameLog {
+		entry.Message = fmt.Sprintf("[%s(%d)] %s", function, line, entry.Message)
+	}
 
-    return nil
+	return nil
 }
 
 func (hook *LineNumLogrusHook) findCaller(skip int) (string, string, int) {
-    var (
-        pc       uintptr
-        file     string
-        function string
-        line     int
-    )
-    for i := 0; i < 10; i++ {
-        pc, file, line = hook.getCaller(skip + i)
-        if !strings.HasPrefix(file, "logrus/") {
-            break
-        }
-    }
-    if pc != 0 && hook.EnableFuncNameLog {
-        frames := runtime.CallersFrames([]uintptr{pc})
-        frame, _ := frames.Next()
-        function = frame.Function
-    }
+	var (
+		pc       uintptr
+		file     string
+		function string
+		line     int
+	)
+	for i := 0; i < 10; i++ {
+		pc, file, line = hook.getCaller(skip + i)
+		if !strings.HasPrefix(file, "logrus/") {
+			break
+		}
+		for _, v := range hook.SkipPrefixs {
+			if !strings.HasPrefix(file, v) {
+				break
+			}
+		}
 
-    return file, function, line
+	}
+	if pc != 0 && hook.EnableFuncNameLog {
+		frames := runtime.CallersFrames([]uintptr{pc})
+		frame, _ := frames.Next()
+		function = frame.Function
+	}
+
+	return file, function, line
 }
 
 func (hook *LineNumLogrusHook) getCaller(skip int) (uintptr, string, int) {
-    pc, file, line, ok := runtime.Caller(skip)
-    if !ok {
-        return 0, "", 0
-    }
+	pc, file, line, ok := runtime.Caller(skip)
+	if !ok {
+		return 0, "", 0
+	}
 
-    n := 0
-    for i := len(file) - 1; i > 0; i-- {
-        if file[i] == '/' {
-            n += 1
-            if n >= 2 {
-                file = file[i+1:]
-                break
-            }
-        }
-    }
+	n := 0
+	for i := len(file) - 1; i > 0; i-- {
+		if file[i] == '/' {
+			n += 1
+			if n >= 2 {
+				file = file[i+1:]
+				break
+			}
+		}
+	}
 
-    return pc, file, line
+	return pc, file, line
 }
